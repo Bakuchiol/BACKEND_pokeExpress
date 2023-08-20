6066544a27bf3a0ea9431edfb425c467bf054d2e
Lynette Cargo <lynettecargo@gmail.com>
2023-08-19 19:50:50 -0500

    cards

--- views/Index.jsx
+++ views/Index.jsx
@@ -5,39 +5,18 @@ function Index(props) {
 
     const pokemon = props.pokemon
 
-    const bg = {
-        backgroundColor: "#f5f5f5",
-        "textAlign": "center"
-    }
-    const center = {
-        "listStyle" : "none"
-    }
-    const myStyle = {
-        color: "crimson",
-        "margin": "20px",
-        "padding": "30px"
-    };
-    const aTag = {
-        color: "royalblue",
-        'textDecoration' : 'none',
-
-    }
-    const style = {
-        color: "crimson",
-        'textDecoration' : 'none',
-        'margin' : '20px',
-        "fontSize" : '20px'
-    }
-
   return (
     <Default>
-    <div style={bg}>
-      <h1 style={myStyle}>See All The Pokemon!</h1>
-      <ul style={center}>
+    <div id='indexWrapper'>
+        <a href='/pokemon/new'>
+            <h1  className='addNew'>Add New Pokemon!</h1>
+        </a>
+      <div className="pokemonList">
+      <ul className='pokemonInfo'>
       {pokemon.map((poke,i) => {
           return (
             <div key={i} className="editPokemon">
-                <a href={`/pokemon/${poke._id}`} style={aTag} key={i}>
+                <a href={`/pokemon/${poke._id}`} key={i}>
                     <li>{poke.name.charAt(0).toUpperCase() + poke.name.slice(1)}</li>
                 </a>
 
@@ -46,18 +25,17 @@ function Index(props) {
                     <a href={`/pokemon/${poke._id}/edit`}>
                     <button className='button'>Edit</button>
                     </a>
-                </div>
 
                 {/* delete */}
                 <form action={`/pokemon/${poke._id}?_method=DELETE`} method="POST">
                     <input type="submit" value="Delete" className='button'/>
                 </form>
+                </div>
             </div>
         )
     })}
     </ul>
-      <br />
-      <a href='/pokemon/new' style={style}>Add Pokemon</a>
+      </div>
       </div>
     </Default>
     )
